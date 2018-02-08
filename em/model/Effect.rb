08c4f909329bb00a913d60ccdc99eb129163881c
Razania <j.ferreira1215@gmail.com>
2018-02-08 16:02:37 +0100

    Model - Effets
    Tacos salade tomate oignon, viande kebab et steack-haché, sauce blanche

--- em/model/Effect.rb
+++ em/model/Effect.rb
@@ -1,5 +1,6 @@
 class Effect
-  attr_reader(:name, :duration,:starting_time)
+  attr_accessor(:starting_time)
+  attr_reader(:name, :duration)
   ##
   # name nom de l'effet
   # duration durée en seconde de l'effet
@@ -18,23 +19,23 @@ class Effect
     return Time.now > @starting_time + @duration
   end
   
-  def getEffect(nameEffect)
+  def self.getEffect(nameEffect)
     case nameEffect
     when "slow"
-      effect = SLOW.clone
+      effet = SLOW.clone
     when "root"
-      effect = ROOT.clone
+      effet = ROOT.clone
     when "blind"
-      effect = BLIND.clone
+      effet = BLIND.clone
     when "weakness"
-      effect = WEAKNESS.clone
+      effet = WEAKNESS.clone
     when "strength"
-      effect = STRENGTH.clone
+      effet = STRENGTH.clone
     when "speed"
-      effect = SPEED.clone
+      effet = SPEED.clone
     end
-    effect.starting_time = Time.now
-    return effect
+    effet.starting_time = Time.now
+    return effet
   end
   
   SLOW = Effect.new("slow", 5)
@@ -44,5 +45,3 @@ class Effect
   STRENGTH = Effect.new("strength", 5)
   SPEED = Effect.new("speed", 5)
 end
-
-puts Effect::SLOW
